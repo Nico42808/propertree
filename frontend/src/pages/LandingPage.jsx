3e67b4306da6108629068a6f3d666d64406abbec
Nico42808 <vetter@immobilien-vetter.com>
2026-08-08 15:12:33 +0200

    Add LandingPage component for Propertree

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,48 @@
+LANDINGPAGE
+
+            PROPERTREE
+
+Your property.
+Professionally managed.
+
+[ Get Started ]    [ Owner Login ]
+
+
+      ↓
+
+
+Everything your property needs
+in one place.
+
+
+ASSETS
+Keep your properties, documents
+and information organized.
+
+SERVICES
+Book maintenance, cleaning,
+inspections and other services.
+
+MANAGEMENT
+We coordinate the people and
+tasks required to keep your
+property running.
+
+
+      ↓
+
+
+How Propertree works
+
+01  Add your property
+02  Choose your services
+03  We take care of the rest
+
+
+      ↓
+
+
+Your property.
+Always taken care of.
+
+[ Start with Propertree ]
