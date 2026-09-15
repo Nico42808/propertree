33a447ac69fa032ff89dfbc7da2782d5ea5bce4d
Nico42808 <vetter@immobilien-vetter.com>
2026-09-15 08:43:33 +0200

    Apply landing page visual language to subpages

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -2,15 +2,18 @@
  * MainLayout - Main application layout with Navbar and Footer
  */
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const MainLayout = () => {
+  const location = useLocation();
+  const isLanding = location.pathname === '/';
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col bg-white text-propertree-dark">
       <Navbar />
-      <main className="flex-grow">
+      <main className={`flex-grow ${isLanding ? '' : 'propertree-subpage'}`}>
         <Outlet />
       </main>
       <Footer />
@@ -19,7 +22,3 @@ const MainLayout = () => {
 };
 
 export default MainLayout;
-
-
-
-
