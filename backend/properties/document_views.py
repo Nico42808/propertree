b298e2ed42063b3fc310771dbbcf227c64c3b96c
Nico42808 <vetter@immobilien-vetter.com>
2026-09-16 23:22:29 +0200

    Add property document API

---
+++ backend/properties/document_views.py
@@ -0,0 +1,61 @@
+from django.http import FileResponse, Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
+from .models import Property
+from .documents import PropertyDocument
+from .document_serializers import PropertyDocumentSerializer, PropertyDocumentUploadSerializer
+
+
+def _can_manage_property(user, property_obj):
+    return getattr(user, 'role', None) == 'admin' or property_obj.landlord_id == user.id
+
+
+class PropertyDocumentListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_property(self):
+        property_obj = get_object_or_404(Property, pk=self.kwargs['property_id'])
+        if not _can_manage_property(self.request.user, property_obj):
+            raise PermissionDenied('You do not have access to this property.')
+        return property_obj
+
+    def get_queryset(self):
+        return PropertyDocument.objects.filter(property=self.get_property()).order_by('-created_at')
+
+    def get_serializer_class(self):
+        return PropertyDocumentUploadSerializer if self.request.method == 'POST' else PropertyDocumentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(property=self.get_property(), uploaded_by=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        document = PropertyDocument.objects.get(pk=response.data['id']) if response.data.get('id') else self.get_queryset().first()
+        return type(response)(PropertyDocumentSerializer(document, context={'request': request}).data, status=response.status_code, headers=response.headers)
+
+
+class PropertyDocumentDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = PropertyDocument.objects.all()
+
+    def perform_destroy(self, instance):
+        if not _can_manage_property(self.request.user, instance.property):
+            raise PermissionDenied('You do not have access to this property.')
+        if instance.file:
+            instance.file.delete(save=False)
+        instance.delete()
+
+
+class PropertyDocumentDownloadView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        document = get_object_or_404(PropertyDocument.objects.select_related('property'), pk=pk)
+        if not _can_manage_property(request.user, document.property):
+            raise PermissionDenied('You do not have access to this property.')
+        if not document.file:
+            raise Http404('File not found')
+        return FileResponse(document.file.open('rb'), as_attachment=True, filename=document.file.name.split('/')[-1])
